2f6fe5cd144840e01376f4c9d96b3c8d7c596839
BrunoTruber <brunotruber45@gmail.com>
2021-09-13 20:30:54 -0300

    atualização

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,19 +4,16 @@ const AirplaneController = require("../controllers/AirplaneController");
 const AirplaneMiddleware = require("../middlewares/AirplaneMiddlewares");
 
 routes.get("/airplanes", AirplaneController.getAll);
-routes.get(
-  "/airplanes/:id",
+routes.get("/airplanes/:id",
   AirplaneMiddleware.validaID,
   AirplaneController.getById
 );
 routes.post("/airplanes", AirplaneController.create);
-routes.put(
-  "/airplanes/:id",
+routes.put("/airplanes/:id",
   AirplaneMiddleware.validaID,
   AirplaneController.update
 );
-routes.delete(
-  "/airplanes/:id",
+routes.delete("/airplanes/:id",
   AirplaneMiddleware.validaID,
   AirplaneController.del
 );
