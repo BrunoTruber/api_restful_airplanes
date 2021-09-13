84aa5db1ea7d9c1a6ba8b57a4596729e467f19b6
BrunoTruber <brunotruber45@gmail.com>
2021-09-13 19:59:33 -0300

    cors

--- src/middlewares/AirplaneMiddlewares.js
+++ src/middlewares/AirplaneMiddlewares.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose");
 const Airplane = require("../models/Airplane");
+const cors = require("cors");
+const express = require("express");
+
+
+const app = express();
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    app.use(cors());
+
+    next();
+});
 
 const validaID = async (req, res, next) => {
   const { id } = req.params;
