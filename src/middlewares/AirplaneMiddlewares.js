5bf5c7ee512747323b9576f733e1cdf26bdf7cc9
BrunoTruber <brunotruber45@gmail.com>
2021-09-14 00:09:06 -0300

    new cors

--- src/middlewares/AirplaneMiddlewares.js
+++ src/middlewares/AirplaneMiddlewares.js
@@ -7,19 +7,11 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-//   // liberar o CORS em todas as requisições
-// app.use(cors());
-//   //ativar todos os pre-flights
-// app.options('*', cors());
+  // liberar o CORS em todas as requisições
+app.use(cors());
+  //ativar todos os pre-flights
+app.options('*', cors());
 
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    app.use(cors());
-    next();
-});
 
 const validaID = async (req, res, next) => {
   const { id } = req.params;
