2d419133cf87a6848e18e99fed6b726e45c00670
BrunoTruber <brunotruber45@gmail.com>
2021-09-13 23:55:06 -0300

    new cors

--- src/middlewares/AirplaneMiddlewares.js
+++ src/middlewares/AirplaneMiddlewares.js
@@ -8,17 +8,14 @@ require("express-async-errors");
 const app = express();
 
 
-app.all("/*", (req, res, next) => {
-	res.header(
-	"Access-Control-Allow-Origin", "*");
-
-	res.header("Access-Control-Allow-Methods", "*");
-
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-		next();
-	});
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 const validaID = async (req, res, next) => {
   const { id } = req.params;
